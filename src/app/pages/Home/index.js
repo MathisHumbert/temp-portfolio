20a6a13d94bdec6b6b1573001e9c7cec1ae4f15a
MathisHumbert <mathishumbert71@gmail.com>
2024-06-17 15:24:27 +0200

    added correct colors and paris time

--- src/app/pages/Home/index.js
+++ src/app/pages/Home/index.js
@@ -8,6 +8,7 @@ export default class Home extends Page {
       element: '.home',
       elements: {
         wrapper: '.home__wrapper',
+        time: document.querySelector('.nav__time'),
       },
     });
   }
@@ -25,5 +26,43 @@ export default class Home extends Page {
     this.element.classList.remove(this.classes.active);
 
     return super.hide();
+  }
+
+  calcTime(offset) {
+    const d = new Date();
+
+    const utc = d.getTime() + d.getTimezoneOffset() * 60000;
+
+    const nd = new Date(utc + 3600000 * offset);
+
+    let hours = nd.getHours();
+    let minutes = nd.getMinutes();
+
+    if (minutes === this.minutes) return;
+
+    this.minutes = minutes;
+
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    if (this.elements.time) {
+      this.elements.time.textContent = `${hours}:${minutes} ${
+        hours > 12 ? 'pm' : 'am'
+      }`;
+    }
+  }
+
+  /**
+   * Loop.
+   */
+  update() {
+    super.update();
+
+    this.calcTime(2);
   }
 }
